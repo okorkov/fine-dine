3eec4b464d0ef8bd61aa5e7f4dbe061e365d141b
okorkov <alex.okorkov@gmail.com>
2021-01-28 10:53:00 -0800

    slot edit,delete functionality added

--- app/controllers/slots_controller.rb
+++ app/controllers/slots_controller.rb
@@ -15,7 +15,7 @@ class SlotsController < ApplicationController
 
   def create
     counter = 1
-    while counter < params[:how_many].to_i
+    while counter <= params[:how_many].to_i
       slot = Slot.create(slot_params)
       counter += 1
     end
@@ -27,11 +27,19 @@ class SlotsController < ApplicationController
   end
 
   def edit
-
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @slot = Slot.find(params[:id])
   end
 
   def update
+    slot = Slot.find(params[:id])
+    slot.update(slot_params)
+    redirect_to restaurant_slots_path(current_restaurant)
+  end
 
+  def destroy
+    Slot.find(params[:id]).destroy
+    redirect_to restaurant_slots_path(current_restaurant)
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
   root  'welcome#welcome'
   resources :restaurant_sessions, only: [:new, :create, :destroy]
   # resources :guests, only: [:index, :new, :create]
-  resources :restaurants, only: [:index, :new, :show, :destroy] do
-    resources :slots
-  end
   resources :slots, only: [:create]
+  resources :restaurants, only: [:index, :new, :show, :destroy] do
+    resources :slots, only: [:new, :edit, :create, :update, :destroy, :index]
+  end
+  
   
 end
