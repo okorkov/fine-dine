6a52a8f55ee3e281470f14a6bcd078912ab16043
okorkov <alex.okorkov@gmail.com>
2021-01-28 16:15:17 -0800

    sessions route, controller views addeed

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+
+  layout 'welcome'
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def destroy
+
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root  'welcome#welcome'
+  resources :sessions, only: [:new, :create, :destroy]
   resources :restaurant_sessions, only: [:new, :create, :destroy]
   resources :slots, only: [:create]
   resources :restaurants, only: [:index, :new, :show, :destroy, :edit, :update, :create] do
