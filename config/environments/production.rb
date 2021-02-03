1ccd4599f57a38580360e37a95d3c5614fe383e8
okorkov <alex.okorkov@gmail.com>
2021-02-03 15:05:27 -0800

    mailer config added to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,4 +117,24 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  config.action_mailer.delivery_method = :sendmail
+  # Defaults to:
+  # config.action_mailer.sendmail_settings = {
+  #   location: '/usr/sbin/sendmail',
+  #   arguments: '-i'
+  # }
+  config.action_mailer.delivery_method = :smtp
+  host = 'fine-dine.herokuapp.com/'
+  config.action_mailer.default_url_options = { :host => 'fine-dine.herokuapp.com/', protocol: 'http' }
+  
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => "finedineadm@gmail.com",
+    :password             => ENV["GMAIL_PASSWORD"],
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
+  end
 end
