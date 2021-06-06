b11009ea8abd513359562b66381737fac95a2104
okorkov <alex.okorkov@gmail.com>
2021-06-05 21:43:24 -0700

    removed individuals heades from every controller before action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
 
   include ApplicationHelper 
 
-  before_action :set_headers
+  # before_action :set_headers
   # skip_before_action :verify_authenticity_token
 
 end
